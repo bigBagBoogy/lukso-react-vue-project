beeb6cfb0c4fb6949d2487383f424d9965a03307
maarten <van_den_bergmaarten@hotmail.com>
2023-10-30 18:55:42 +0100

    before big refactoring connect

--- src/ConnectUPButton.jsx
+++ src/ConnectUPButton.jsx
@@ -14,6 +14,7 @@ function ConnectUPButton() {
       if (window.ethereum.selectedAddress) {
         setIsConnected(true);
       } else {
+        console.log("line17");
         // UP extension not available or not installed
         alert("Please install the LUKSO UP Browser Extension to connect.");
       }
