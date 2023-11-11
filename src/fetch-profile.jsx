2e47d2b217faf831ceee532a87313cbb753d2877
maarten <van_den_bergmaarten@hotmail.com>
2023-11-11 08:50:31 +0100

    cleaned up code

--- src/fetch-profile.jsx
+++ src/fetch-profile.jsx
@@ -2,15 +2,12 @@
 import React, { useState, useEffect } from "react";
 
 async function fetchProfileFromBackend(userAddress, setData) {
-  try {
-   
+  try {   
     console.log("userAddress:", userAddress);
     const response = await fetch(
       `http://localhost:5000/fetch-profile/${userAddress}`
-    );
-    
+    );    
     console.log("Response Status:", response.status);
-
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
@@ -19,20 +16,17 @@ async function fetchProfileFromBackend(userAddress, setData) {
     console.log("Full Response from backend:", returnedProfileObject);
 
     setData(returnedProfileObject);
-    ///// continue here Maarten! ///////
+    // constructing ipfs url
     const ipfsUrl = "https://universalpage.dev/api/ipfs/" + JSON.stringify(returnedProfileObject.profileData.profileData[1]?.value.url).replace(/["']/g, "").replace("ipfs://", "");
     fetch(ipfsUrl)
       .then(response => response.json())
       .then(ipfsProfile => {
-// Access elements of the JSON object here
+  // Access elements of the JSON object here
      console.log(ipfsProfile);
      console.log('name:', ipfsProfile.LSP3Profile.name);
      console.log('description 2:', ipfsProfile.LSP3Profile.description);
-// Continue with your code
-})
-.catch(error => console.error('Error fetching ipfsProfile:', error));
-///////////  end   end    end   ///////////////
-
+  })
+      .catch(error => console.error('Error fetching ipfsProfile:', error));
   } catch (error) {
     console.error("Error fetching profile from backend:", error);
   }
@@ -67,22 +61,7 @@ async function fetchProfileFromBackend(userAddress, setData) {
           <p>Profile data: {JSON.stringify(data.profileData.issuedAssetsDataKey.name
 )}</p>
 <p>SupportedStandards in LSP3Profile: {JSON.stringify(data.profileData.profileData[0].value)}</p>
-
 <p>ipfs Profile: {"https://universalpage.dev/api/ipfs/" + JSON.stringify(data.profileData.profileData[1]?.value.url).replace(/["']/g, "").replace("ipfs://", "")}</p>
-
-          
-  {/* <pre>{JSON.stringify(data.profileData, null, 2)}</pre>
-
-  <h2>Issued Assets Data</h2>
-  <pre>{JSON.stringify(data.issuedAssetsDataKey, null, 2)}</pre>
-
-  <h2>Received Assets Data</h2>
-  <pre>{JSON.stringify(data.receivedAssetsDataKey, null, 2)}</pre>
-
-  <h2>Universal Receiver Data</h2>
-  <pre>{JSON.stringify(data.universalReceiverDataKey, null, 2)}</pre> */}
-
-        
         </div>
       ) : (
         <p>Loading profile data...</p>
