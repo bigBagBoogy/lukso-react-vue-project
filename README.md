3cc553f3ee8c74376218dbae4c51c33453572628
maarten <van_den_bergmaarten@hotmail.com>
2023-11-11 15:29:29 +0100

    added fetch group token

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ in connectWallet() we get access to users Address
     // const address = "0x9139def55c73c12bcda9c44f12326686e3948634"; // lukso example
     // const address = "0x163CF6D68Fb7287e032Eb7d1a797E737174985c1"; // Heavy metal Group
     // const address = "0x3F0350EaFc25Cc9185a77394B7E2440ec002e466"; // my UP
+
+This might be the address of Lukso's LSP7 minter contract:
+0x23Ba084F21fC53a765c95Ae5DB97c58ddAAE93aA
+or this one:
+0x8e5cca1769ae60de02bea8ff67ee2af6afb53a510c707ed8e15878dd10cee022 marked as parent
---
+++ src/fetch-groupTokens.jsx
@@ -0,0 +1,77 @@
+// fetch-profile.jsx   copy copy copy
+import React, { useState, useEffect } from "react";
+
+async function fetchProfileFromBackend(userAddress, setData) {
+  try {   
+    console.log("userAddress:", userAddress);
+    const response = await fetch(
+      `http://localhost:5000/fetch-profile/${userAddress}`
+    );    
+    console.log("Response Status:", response.status);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const returnedProfileObject = await response.json();
+    console.log("Full Response from backend:", returnedProfileObject);
+
+    
+    // constructing ipfs url
+    const ipfsUrl = "https://universalpage.dev/api/ipfs/" + JSON.stringify(returnedProfileObject.profileData.profileData[1]?.value.url).replace(/["']/g, "").replace("ipfs://", "");
+    fetch(ipfsUrl)
+      .then(response => response.json())
+      .then(ipfsProfile => {
+  // Access elements of the JSON object here
+     console.log(ipfsProfile);
+     console.log('name:', ipfsProfile.LSP3Profile.name);
+     console.log('description 2:', ipfsProfile.LSP3Profile.description);     
+  })  
+      .catch(error => console.error('Error fetching ipfsProfile:', error));
+
+      return returnedProfileObject;
+  } catch (error) {
+    console.error("Error fetching profile from backend:", error);
+  }
+
+}
+
+
+
+const FetchProfile = () => {
+  const [data, setData] = useState(null);
+  const userAddress = "0x9139def55c73c12bcda9c44f12326686e3948634"; // lukso example
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const fetchedData = await fetchProfileFromBackend(userAddress, setData);
+        if (fetchedData) {
+          setData(fetchedData);
+        }
+      } catch (error) {
+        console.error("Error fetching data from backend:", error);
+      }
+    };
+    fetchProfile(); // Call the function when the component mounts
+  }, [userAddress, setData]); 
+
+
+  return (
+    <div>
+      {data ? (
+        <div>
+          {/* <p>Profile from the backend:</p> */}
+          {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+          <p>Profile data: {JSON.stringify(data.profileData.issuedAssetsDataKey.name
+)}</p>
+<p>SupportedStandards in LSP3Profile: {JSON.stringify(data.profileData.profileData[0].value)}</p>
+<p>ipfs Profile: {"https://universalpage.dev/api/ipfs/" + JSON.stringify(data.profileData.profileData[1]?.value.url).replace(/["']/g, "").replace("ipfs://", "")}</p>
+        </div>
+      ) : (
+        <p>Loading profile data...</p>
+      )}
+    </div>
+  );
+};
+
+export { FetchProfile, fetchProfileFromBackend };
